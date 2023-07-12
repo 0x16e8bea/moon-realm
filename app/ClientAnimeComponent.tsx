33731a04461b71bc6dfed00145c519cbf20960e6
Mikkel Mogensen <0x16e8bea@gmail.com>
2023-07-12 22:47:38 +0200

    Fix issue with use of class name

--- app/ClientAnimeComponent.tsx
+++ app/ClientAnimeComponent.tsx
@@ -35,7 +35,7 @@ export default function ClientAnimeComponent() {
 			<h1>
 				<div ref={logoRef}>
 					<div id="lineDrawing">
-						<svg width="125" height="101" viewBox="0 0 125 101" fill="transparent" xmlns="http://www.w3.org/2000/svg" class="lines">
+					<svg width="125" height="101" viewBox="0 0 125 101" fill="transparent" xmlns="http://www.w3.org/2000/svg" className="lines">
 							<g clip-path="url(#clip0_2_2)">
 								<path d="M123.2 41.4C123.2 52.4 118.7 62.4 111.5 69.6C104.3 76.8 94.5 81.2 83.6 81.3C96.5 72.7 105 58 105 41.4C105 28.2 99.7 16.2 91 7.60001C88.8 5.40001 86.3 3.30001 83.7 1.60001C105.5 1.70001 123.2 19.5 123.2 41.4Z" fill="transparent" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 								<path d="M92.5 99.3L62.8 41.3" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
